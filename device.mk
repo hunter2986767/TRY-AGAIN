95c2e379ad5a787df6a74e7696de1211b7d06606
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-06-11 17:12:18 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,7 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 AB_OTA_PARTITIONS += \
