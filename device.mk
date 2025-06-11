5cb7050dfab3f400096d1905663c65ce9979702c
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-06-11 16:24:53 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -75,12 +75,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6877
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6877 \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
